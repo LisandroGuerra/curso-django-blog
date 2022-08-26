9d46160d7a6b764a7f60f1df1974ff8ce52dd586
Lisandro Guerra <lisandro.digital@gmail.com>
2022-08-26 17:00:56 -0300

    django files

---
+++ website/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from website.models import Post
+
+
+admin.site.register(Post)
---
+++ website/models.py
@@ -0,0 +1,9 @@
+from turtle import title
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=300)
+    author = models.CharField(max_length=30)
+    content = models.TextField()
