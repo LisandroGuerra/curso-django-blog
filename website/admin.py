fb564f33ac9321c8c1bff646d44ac968a3041c1e
Lisandro Guerra <lisandro.digital@gmail.com>
2022-08-29 20:08:03 -0300

    link templates to models an configure static

--- website/admin.py
+++ website/admin.py
@@ -4,7 +4,10 @@ from website.models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'sub_title')
+    list_display = ('title', 'sub_title', 'author', 'categories', 'approved')
+
+    def get_queryset(self, request):
+        return Post.objects.filter(approved=True)
 
 
 admin.site.register(Post, PostAdmin)
--- website/models.py
+++ website/models.py
@@ -2,11 +2,26 @@ from turtle import title
 from django.db import models
 
 
+class Categories(models.TextChoices):
+    LX = 'LX', 'Linux'
+    AR = 'AR', 'Arduino'
+    RB = 'RB', 'RaspberryPI'
+    VI = 'VI', 'VI and VIM'
+    PY = 'PY', 'Python'
+    DJ = 'DJ', 'Django'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=300)
     author = models.CharField(max_length=30)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categories.choices,
+        default=Categories.LX,
+    )
+    approved = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.title
